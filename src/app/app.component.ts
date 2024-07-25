632c65bdfaa8b9791f0290ad7b88427eb8b51aad
Edwin HC <edwinrhc@gmail.com>
2024-07-25 16:53:10 -0500

    módulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {MainContentComponent} from "./main-content/main-content.component";
+import {CardSectionComponent} from "./card-section/card-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, NavbarComponent, MainContentComponent, CardSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/card-section/card-section.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-section.component.html',
+  styleUrl: './card-section.component.css'
+})
+export class CardSectionComponent implements OnInit{
+
+  cards =  [
+    { title: '1. Regístrese en el Sifonavi', image: '/assets/img/web.png' ,url: 'https://apps4.mineco.gob.pe/registro0/registro.htm'},
+    { title: '2. Registro de Historial Laboral F1', image: '/assets/img/web.png',url:'https://apps4.mineco.gob.pe/registro1/pages/index.jsp' },
+    { title: '3. Estado de Registro', image: '/assets/img/web.png',url:'https://www.fonavi-st.gob.pe/sifonavic1/'},
+    { title: '4. Consulte Aquí si es parte del Primer Grupo, Reintegro -Abril 2024', image: '/assets/img/web.png',url:'https://www.fonavi-st.gob.pe/sifonavic2/' },
+    { title: '5. Consulte su CERAD 2023 - 2024', image: '/assets/img/web.png',url:'https://www.fonavi-st.gob.pe/sifonavic5/' },
+    { title: '6. Pendientes de Cobro Grupos de Pago N° 1 al 20', image: '/assets/img/web.png',url:'https://www.fonavi-st.gob.pe/sifonavic3/'},
+    { title: '7. DECRETO SUPREMO N° 280-2023-EF Publicación de Herederos Beneficiarios', image: '/assets/img/web.png',url:'https://www.fonavi-st.gob.pe/sifonavic4/' },
+    { title: 'Buscar Empleadores', image: '/assets/img/web.png',url:'https://apps4.mineco.gob.pe/busqe/busca_empleador.htm' },
+    { title: 'Portal Web', image: '/assets/img/web.png',url: 'https://fonavi-st.pe/' }
+  ]
+
+  constructor() {
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}
